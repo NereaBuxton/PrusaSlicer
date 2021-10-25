bf02270d83108ee2b15fd03a7df52f61c568de20
n8bot <22458343+n8bot@users.noreply.github.com>
2021-10-25 11:49:56 -0700

    Tweak

--- src/libslic3r/PrintObject.cpp
+++ src/libslic3r/PrintObject.cpp
@@ -1328,7 +1328,7 @@ void PrintObject::discover_vertical_shells()
 #if 1
                     // Intentionally inflate a bit more than how much the region has been shrunk, 
                     // so there will be some overlap between this solid infill and the other infill regions (mainly the sparse infill).
-                    shell = opening(union_(shell), 0.5f * min_perimeter_infill_spacing, 0.8f * min_perimeter_infill_spacing, ClipperLib::jtSquare);
+                    shell = opening(union_(shell), 0.5f * min_perimeter_infill_spacing, 2.75f * min_perimeter_infill_spacing, ClipperLib::jtRound);
                     if (shell.empty())
                         continue;
 #else
