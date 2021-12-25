42c3a23fef7202ab9c997546c2f9c6d60d7e42ff
n8bot <22458343+n8bot@users.noreply.github.com>
2021-12-24 22:58:53 -0800

    Set overlay button colour based on dark mode

--- src/slic3r/GUI/GLTexture.cpp
+++ src/slic3r/GUI/GLTexture.cpp
@@ -1,6 +1,8 @@
 #include "libslic3r/libslic3r.h"
 #include "GLTexture.hpp"
 
+#include "GUI_App.hpp"
+
 #include "3DScene.hpp"
 #include "OpenGLManager.hpp"
 
@@ -218,7 +220,7 @@ bool GLTexture::load_from_svg_files_as_sprites_array(const std::vector<std::stri
         for (int i = 0; i < sprite_n_pixels; ++i) {
             int offset = i * 4;
             if (sprite_white_only_data.data()[offset] != 0)
-                ::memset((void*)&sprite_white_only_data.data()[offset], 192, 3);
+                ::memset((void*)&sprite_white_only_data.data()[offset], wxGetApp().dark_mode() ? 64 : 240, 3);
         }
 
         // makes gray only copy of the sprite
@@ -226,7 +228,7 @@ bool GLTexture::load_from_svg_files_as_sprites_array(const std::vector<std::stri
         for (int i = 0; i < sprite_n_pixels; ++i) {
             int offset = i * 4;
             if (sprite_gray_only_data.data()[offset] != 0)
-                ::memset((void*)&sprite_gray_only_data.data()[offset], 64, 3);
+                ::memset((void*)&sprite_gray_only_data.data()[offset], wxGetApp().dark_mode() ? 43 : 64, 3);
         }
 
         int sprite_offset_px = sprite_id * (int)sprite_size_px_ex * m_width;
